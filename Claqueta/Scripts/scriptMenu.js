7c258a14ce5f0f49004f71e0191e887ad635cf5a
Daniela <danidiaz420@gmail.com>
2018-11-02 19:42:34 -0500

    cm-Master
    Master+menu desplegable

---
+++ Claqueta/Scripts/scriptMenu.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    $(".submenu").hide();
+    $(".contenedor--menu").hide();
+
+    $(".icono").click(function () {
+        $(".contenedor--menu").animate({
+            width: "toggle"
+
+        });
+    });
+
+    $(".submenu").before(innerHTML = "\u25bc");
+    $('.submenu')
+    //despliega solo el submenu de ese menu concreto
+    $('.menu__enlace').click(function (event) {
+        var elem = $(this).next();
+
+        if (elem.is('ul')) {
+            event.preventDefault();
+            elem.slideToggle();
+        }
+    });
+
+});//fin de la funcion ready
