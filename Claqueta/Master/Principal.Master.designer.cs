00fae46ba0e2bfb59eaf3dc6cb04961a794f3389
Daniela <danidiaz420@gmail.com>
2018-11-08 20:12:08 -0500

    cm search bar
    barra de busqueda

--- Claqueta/Master/Principal.Master.designer.cs
+++ Claqueta/Master/Principal.Master.designer.cs
@@ -40,15 +40,6 @@ namespace Claqueta {
         protected global::System.Web.UI.WebControls.ImageButton imaBtLogo;
         
         /// <summary>
-        /// imaBtSearch control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ImageButton imaBtSearch;
-        
-        /// <summary>
         /// imaUser control.
         /// </summary>
         /// <remarks>
