91757621af647f68244d0f3100f35ac35ce4e545
Daniela <danidiaz420@gmail.com>
2018-11-19 15:57:23 -0500

    CM validacion
    valida si un usuario o un correo ya han sido registrados

--- Claqueta/Modulos/Registro.aspx.cs
+++ Claqueta/Modulos/Registro.aspx.cs
@@ -12,6 +12,11 @@ namespace Claqueta.Modulos
 {
     public partial class Registro : System.Web.UI.Page
     {
+        bool validarUsuario = false;
+        bool validarCorreo = false;
+        bool uRegistrado = false;
+        bool cRegistrado = false;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -34,19 +39,96 @@ namespace Claqueta.Modulos
             {
                 try
                 {
-                var sqlConn = new SqlConnection(ConfigurationManager.ConnectionStrings["claqueta_dbConnectionString"].ConnectionString);
-                var strSQL = "INSERT INTO registro VALUES ('" + usuario + "','" + tipoDocumento + "','" + Documento + "','"
-                    + Nombre + "','" + Apellido + "','" + Correo + "', '" + Correo2 + "', '" + Contrasena + "', '" + Contrasena2 + "')";
-                var com = new SqlCommand(strSQL, sqlConn);
-                sqlConn.Open();
-                com.ExecuteNonQuery();
-                sqlConn.Close();
-                 }
-                 catch (Exception ex)
-                 {
+                    var sqlConn = new SqlConnection(ConfigurationManager.ConnectionStrings["claqueta_dbConnectionString"].ConnectionString);
+                    var strSQL = "INSERT INTO registro VALUES ('" + usuario + "','" + tipoDocumento + "','" + Documento + "','"
+                        + Nombre + "','" + Apellido + "','" + Correo + "', '" + Correo2 + "', '" + Contrasena + "', '" + Contrasena2 + "')";
+                    var com = new SqlCommand(strSQL, sqlConn);
+                    sqlConn.Open();
 
-                 }
-                 limpiarCampos();
+                    if (usuario == "" || Documento == "" || Nombre == "" || Apellido == "" || Correo == "" || Correo2 == "" || Contrasena == "" || Contrasena2 == "")
+                    {
+                        sqlConn.Close();
+                    }
+
+                    else if (Correo != Correo2 || Contrasena != Contrasena2)
+                    {
+                        sqlConn.Close();
+                    }
+
+                    else if (validarUsuario==false && validarCorreo == false)
+                    {
+                        var sqlConn2 = new SqlConnection(ConfigurationManager.ConnectionStrings["claqueta_dbConnectionString"].ConnectionString);
+                        using (SqlCommand cmd2 = new SqlCommand())
+                        {
+                            sqlConn2.Open();
+
+                            cmd2.CommandText = "SELECT * FROM [registro] ";
+                            cmd2.Connection = sqlConn2;
+
+                            SqlDataReader rd = cmd2.ExecuteReader();
+
+                            //valida el usuario
+                            while (rd.Read())
+                            {
+                                if (rd[0].ToString() == usuario)
+                                {
+                                    uRegistrado = true;
+                                    break;
+                                }
+                            }
+
+                            if (uRegistrado == true)
+                            {
+                                Response.Write("<script>alert('El usuario ya esta en uso, por favor ingrese uno válido');</script>");
+                                usuario = string.Empty;
+                            }
+                            else
+                            {
+                                validarUsuario = true;
+                            }
+
+                            //valida el usuario
+                            while (rd.Read())
+                            {
+                                if (rd[5].ToString() == Correo)
+                                {
+                                    cRegistrado = true;
+                                    break;
+                                }
+                            }
+
+                            if (cRegistrado == true)
+                            {
+                                Response.Write("<script>alert('El correo ya se encuentra registrado, por favor ingrese uno válido');</script>");
+                                Correo = string.Empty;
+                                Correo2 = string.Empty;
+                            }
+                            else
+                            {
+                                validarCorreo= true;
+                            }
+
+                        }
+
+                        if (validarUsuario==true && validarCorreo == true)
+                        {
+                            Response.Write("<script>alert('Se ha registrado correctamente! Seras redireccionado al inicio de sesión');</script>");
+                            System.Threading.Thread.Sleep(3000);
+                            com.ExecuteNonQuery();
+                            sqlConn.Close();
+                            Response.Redirect("InicioSesion.aspx");
+                            limpiarCampos();
+
+                        }
+                        com.ExecuteNonQuery();
+                    }
+                }
+                
+                catch (Exception ex)
+                {
+
+                }
+
              }
          }
 
@@ -61,7 +143,6 @@ namespace Claqueta.Modulos
              txCorreo2.Value = string.Empty;
              txPassword.Value = string.Empty;
              txPassword2.Value = string.Empty;
-        
             }
 
         protected void btSalir_Click(object sender, EventArgs e)
